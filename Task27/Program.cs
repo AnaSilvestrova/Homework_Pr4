5c802d9e798db9052ce04557ccd5b4dad924504c
AnaSilvestrova <silvestrovaab@gmail.com>
2023-06-26 02:22:00 +0700

    task 27 solved

---
+++ Task27/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Clear();
+
+int GetNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine()); // Get the number.
+    return number;
+}
+
+int GetSumofDig(int number) // Find sum of digits of the number.
+{
+    int digit = 0;
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10; // Summering digits after getting the previous digit?
+        number /=10; // Changing digits.
+    }
+    return sum;
+}
+
+int number = GetNumber();
+int result = GetSumofDig(number);
+
+Console.WriteLine($"Sum of digits of {number} is {result}.");
